6bc4c2bb8abb096d72d64239ae9dd0bb52acee31
pandeyneha0123 <pandeyneha0123>
2023-01-18 17:33:06 +0530

    add

---
+++ problem4.js
@@ -0,0 +1,14 @@
+
+// palindrome number
+var isPalindrome = function(x) {
+    var reverse = 0;
+    var copy = x;
+
+    while (copy > 0) {
+      const digit = copy % 10;
+      reverse = reverse * 10 + digit;
+      copy = ~~(copy / 10); //Math.floor(copy / 10)
+    }
+
+    return reverse == x;
+};
