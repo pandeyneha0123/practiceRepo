0c451410e50c80fbefb49c71de623a061fd6fc5a
pandeyneha0123 <pandeyneha0123>
2023-01-23 12:38:57 +0530

    add

---
+++ problem20.js
@@ -0,0 +1,32 @@
+// 876. Middle of the Linked List
+
+/*
+initial state
+f
+1 -> 2 -> 3 -> 4 -> 5
+s
+
+1st loop
+		  f
+1 -> 2 -> 3 -> 4 -> 5
+     s
+	 
+2nd loop
+		            f
+1 -> 2 -> 3 -> 4 -> 5
+          s
+
+when f reach end of the linked list, s will be at the middle.
+
+f = fast pointer
+s = slow pointer
+*/
+
+var middleNode = function(head) {
+    let fast = slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
