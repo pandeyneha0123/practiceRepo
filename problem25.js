45464ffe4b28099019df29b407c0c13f2ccbdb9e
pandeyneha0123 <pandeyneha0123>
2023-01-24 11:17:24 +0530

    add

---
+++ problem25.js
@@ -0,0 +1,30 @@
+// 232. Implement Queue using Stacks
+
+var MyQueue = function () {
+    this.firstStack = []
+    this.secondStack = []
+  }
+  
+  MyQueue.prototype.push = function (x) {
+    this.firstStack.push(x)
+  }
+  
+  MyQueue.prototype.pop = function () {
+    this._prepare()
+    return this.secondStack.pop()
+  }
+  
+  MyQueue.prototype.peek = function () {
+    this._prepare()
+    return this.secondStack[this.secondStack.length - 1]
+  }
+  
+  MyQueue.prototype.empty = function () {
+    return this.firstStack.length === 0 && this.secondStack.length === 0
+  }
+  
+  MyQueue.prototype._prepare = function () {
+    if (this.secondStack.length === 0)
+      while (this.firstStack.length > 0)
+        this.secondStack.push(this.firstStack.pop())
+  }
